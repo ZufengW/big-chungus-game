ce53f46ad6b8c03d33d0eb3f289393a9d385e88a
Zufeng Wang <zufengw@gmail.com>
2019-01-27 19:26:54 +1100

    Add scroll button to scroll to game canvas

--- src/app/app.ts
+++ src/app/app.ts
@@ -41,6 +41,11 @@ document.getElementById('canvas-div').appendChild(app.view);
 /** p node for displaying loading messages */
 const loadingP = document.getElementById('loading-p');
 
+// When clicked, scroll until canvas is aligned to the top of the visible area
+document.getElementById('scroll-button').onclick = () => {
+  app.view.scrollIntoView(true);
+};
+
 // Load the assets
 loader
   .add(R.MAP_PATH)
